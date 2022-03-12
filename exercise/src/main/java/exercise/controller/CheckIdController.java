9d27609cc727856dee8f135e146547c8986c6b9d
smhrd <smhrd@121.147.185.152>
2022-03-12 09:34:20 +0900

    checkid

---
+++ exercise/src/main/java/exercise/controller/CheckIdController.java
@@ -0,0 +1,28 @@
+package exercise.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import exercise.entity.MemberVO;
+import exercise.model.MemberDAO;
+
+public class CheckIdController implements Controller {
+@Override
+public String requestHandler(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+	
+	String memId = request.getParameter("memId");
+	MemberVO vo = new MemberVO();
+	vo.setMemId(memId);
+	// Model??
+	MemberDAO dao=new MemberDAO();
+	int cnt = dao.checkId(vo);
+	
+	return "cnt";
+	
+}
+}
