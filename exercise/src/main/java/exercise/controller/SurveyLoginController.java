919c8c962de631354d5914f473262b5a038de4f7
smhrd <smhrd@121.147.185.152>
2022-03-12 09:01:48 +0900

    서베이로그인 수정

---
+++ exercise/src/main/java/exercise/controller/SurveyLoginController.java
@@ -0,0 +1,41 @@
+package exercise.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import exercise.entity.MemberVO;
+import exercise.model.MemberDAO;
+
+public class SurveyLoginController implements Controller {
+@Override
+public String requestHandler(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+	String memId = request.getParameter("memId");
+	String memPwd = request.getParameter("memPwd");
+	MemberVO vo = new MemberVO();
+	vo.setMemId(memId);
+	vo.setMemPwd(memPwd);
+	// Model??
+	MemberDAO dao=new MemberDAO();
+	MemberVO memVO = dao.checkLogin(vo);
+	if(memVO!=null) { // 성공
+		HttpSession session= request.getSession(); // 
+		// 객체바인딩(HttpSession)
+		session.setAttribute("memVO", memVO);
+		System.out.println("로그인 성공!");
+		
+	}else if(memVO==null) { // 실패
+		HttpSession session= request.getSession(); // 
+		// 객체바인딩(HttpSession)
+		session.setAttribute("memVO", memVO);
+		System.out.println("로그인 실패!");
+		 return "redirect:/login_result.jsp";
+	}
+	return "redirect:/survey.do";
+	
+}
+}
